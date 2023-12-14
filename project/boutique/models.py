682ff92530979af65671045936edbb88f5515f20
amadoucodeur <amadoucodeur@gmail.com>
2023-12-14 21:03:36 +0000

    les models #2

--- project/boutique/models.py
+++ project/boutique/models.py
@@ -6,11 +6,11 @@ class CustomeUser(AbstractUser):
     phone_number = models.CharField(blank=True, null=True)
     avatar = models.ImageField(upload_to='profile_images/' ,blank=True, null=True)
     bio = models.TextField(blank=True)
-    adress = models.CharField(max_length=200, blank=True)
+    address = models.CharField(max_length=200, blank=True)
     email = models.EmailField(unique=True)
 
     def __str__(self):
-        return self.user.username
+        return self.name
 
 
 class Category(models.Model):
@@ -24,9 +24,11 @@ class Article(models.Model):
     name = models.CharField(max_length=225)
     description = models.TextField()
     add_date = models.DateTimeField(auto_now_add=True)
-    seller = models.ForeignKey(UserProfile, related_name="articles", on_delete=models.CASCADE)
+    seller = models.ForeignKey(CustomeUser, related_name="articles", on_delete=models.CASCADE)
     price = models.IntegerField()
-    qualite = models.TextChoices([])
+    QUALITY_CHOICES = [(1,"1/3"),(2,"2/3"),(3,"3/3")]
+    qualite = models.IntegerField(choices=QUALITY_CHOICES)
+    category = models.ManyToManyField(Category, related_name='category')
 
     def __str__(self):
         return self.name
--- project/chat/models.py
+++ project/chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Chat(models.Model):
+    participants = models.ManyToManyField(get_user_model(), on_delete=models.CASCADE)
+
+class Message(models.Model):
+    sender = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(blank=True)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages')
+
+class Media(models.Model):
+    image = models.ImageField(upload_to='chat_medias/')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='medias', null=True, blank=True)
